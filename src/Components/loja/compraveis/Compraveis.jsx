68158a0c4526776ccea228dffd2d88063c21f75c
Vitor Bulbovas <57118737+Pololokka@users.noreply.github.com>
2023-03-09 16:45:48 -0300

    fix:removido location.reload
    o location.reload estava quebrando a página, então removi
    agora o usuário vai ter dificuldades para saber quanto dinheiro possui exatamente, pois o valor somente é atualizado na mudança/atualização da página, então isso deve ser corrigido no futuro

--- src/Components/loja/compraveis/Compraveis.jsx
+++ src/Components/loja/compraveis/Compraveis.jsx
@@ -38,7 +38,6 @@ const Mercador = ({ equip }) => {
       dinheiroSalvo -= evento.preco;
       const variavelDinheiroSalvo = JSON.stringify(dinheiroSalvo);
       localStorage.setItem("dinheiroRecebido", variavelDinheiroSalvo);
-      window.location.reload();
     }
   };
 
